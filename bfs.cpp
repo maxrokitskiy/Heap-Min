0c9d3f22ee0de9abd123ee944b9c0168db8f7bac
Max Rokitskiy <43040841+maxrokitskiy@users.noreply.github.com>
2019-05-20 22:12:03 +0300

    Update bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -1,64 +1,56 @@
+// Heap min.cpp
+
+//#include "pch.h"
+#include <iostream>
 #include <iostream>
 #include <vector>
-#include <stack>
-#include <queue>
-#include <algorithm>
+
 using namespace std;
-const int INF = 1000000000;
-int main () {
+
+int siftUp(vector<int>& heap, int v) {
+	while ((v > 1) && (heap[v] > heap[v / 2])) {
+		swap(heap[v], heap[v / 2]);
+		v /= 2;
+	}
+	return v;
+}
+
+int siftDown(vector<int>& heap, int v) {
+	while (v * 2 < heap.size()) {
+		int maxIndex = v * 2;
+		if ((v * 2 + 1 < heap.size()) && (heap[v * 2 + 1] > heap[v * 2])) {
+			maxIndex = v * 2 + 1;
+		}
+		if (heap[maxIndex] > heap[v]) {
+			swap(heap[v], heap[maxIndex]);
+			v = maxIndex;
+		}
+		else {
+			break;
+		}
+	}
+	return v;
+}
+
+int main() {
+
 	int n;
 	cin >> n;
-	int a[n][n];
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> a[i][j];
-		}
+	vector<int> a(n + 1);
+	for (int i = 0; i < n; ++i) {
+		cin >> a[i + 1];
 	}
-	int start, end;
-	cin >> start >> end;
-	start--;
-	end--;
-	queue<int> q;
-	q.push(start);
-	vector<int> d(n, INF);
-	vector<int> p(n);
-	p[start] = -1;
-	d[start] = 0;
-	
-		
-	while (q.size() > 0) {
-		
-		int v = q.front();
-		q.pop();
-		
-		if (v == end) {
-			cout << d[end] << endl;
-		}
-		
-		for (int i = 0; i < n; i++) {
-			if (d[i] == INF && a[v][i] == 1) {
-				
-				d[i] = d[v] + 1;
-				q.push(i);
-				p[i] = v;
-			}
-		}
+	int m;
+	cin >> m;
+	for (int i = 0; i < m; ++i) {
+		int v, x;
+		cin >> v >> x;
+		a[v] -= x;
+		cout << siftDown(a, v) << endl;
 	}
-		
-	
-	if (d[end] == INF) {
-		cout << "-1";
-	} else {
-		vector<int> path;
-		for (int v = end; v != -1; v = p[v]) {
-			path.push_back (v);
+	for (int i = 0; i < n; ++i) {
+		cout << a[i + 1] << " ";
 	}
-		reverse (path.begin(), path.end());
-		if (path.size() > 1) {
-			for (int i = 0; i < path.size(); i++) {
-			cout << path[i] + 1 << " ";
-			}
-		}
-		
-	}
+	cout << endl;
+
 }
